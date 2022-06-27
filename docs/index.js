4c3b61bdccd089757bc2d82f539b7da6b0aa3071
Lucas Machado <101845071+Luksiss@users.noreply.github.com>
2022-06-27 16:21:36 -0300

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -1,21 +1,37 @@
 
-
 function card() {
-    document.getElementById("card-info").classList.add('blocko');
-    document.getElementById("pix-info").classList.remove('blocko');
-    document.getElementById("paypal-info").classList.remove('blocko');
+
+    document.getElementById("card-info").classList.add('visible');
+    document.getElementById("pix-info").classList.remove('visible');
+    document.getElementById("paypal-info").classList.remove('visible');
 }
 
 
 function pixo() {
-    document.getElementById("card-info").classList.remove('blocko');
-    document.getElementById("paypal-info").classList.remove('blocko');
-    document.getElementById("pix-info").classList.add('blocko');
+    document.getElementById("card-info").classList.remove('visible');
+    document.getElementById("paypal-info").classList.remove('visible');
+    document.getElementById("pix-info").classList.add('visible');
   }
   
 
   function paypale() {
-    document.getElementById("card-info").classList.remove('blocko');
-    document.getElementById("pix-info").classList.remove('blocko');
-    document.getElementById("paypal-info").classList.add('blocko');
+    document.getElementById("card-info").classList.remove('visible');
+    document.getElementById("pix-info").classList.remove('visible');
+    document.getElementById("paypal-info").classList.add('visible');
   }
+
+  function resetcards() {
+    document.getElementById("card-info").classList.remove('visible');
+    document.getElementById("pix-info").classList.remove('visible');
+    document.getElementById("paypal-info").classList.remove('visible');
+  }
+
+  if (document.getElementById("card-info").classList.contains('visible')){
+
+
+
+
+
+  }
+
+ 
