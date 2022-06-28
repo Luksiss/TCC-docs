908f1577da97cea63a24db64a84c5d0a527b98ba
Lucas Machado <101845071+Luksiss@users.noreply.github.com>
2022-06-28 20:07:13 -0300

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -37,7 +37,29 @@ function pixo() {
     if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "") {
       alert("Por favor preencha o formulrio");
       return false;
+      
     }
-    window.location.href="Index.html";
+
+    
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("submit");
+var modalImg = document.getElementById("img01");
+ img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src = "Vlw.png";
+
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
   }
- 
