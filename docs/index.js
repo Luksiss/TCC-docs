722df2837a785fa4dec538e71e200ce02b6fe44b
Lucas Machado <101845071+Luksiss@users.noreply.github.com>
2022-06-28 18:34:03 -0300

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -26,6 +26,18 @@ function pixo() {
     document.getElementById("paypal-info").classList.remove('visible');
   }
 
-  
 
+
+  
+  function validateForm() {
+    var a = document.forms["Form"]["email"].value;
+    var b = document.forms["Form"]["nome"].value;
+    var c = document.forms["Form"]["password"].value;
+    var d = document.forms["Form"]["password2"].value;
+    if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "") {
+      alert("Por favor preencha o formulrio");
+      return false;
+    }
+    window.location.href="Index.html";
+  }
  
